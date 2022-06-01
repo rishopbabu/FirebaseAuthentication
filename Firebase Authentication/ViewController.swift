71856fae7afdcbba1dae36ed598bd2323c593c67
Rishop Babu <rishop.babu@optisolbusiness.com>
2022-06-01 15:40:40 +0530

    Initial Commit

---
+++ Firebase Authentication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase Authentication
+//
+//  Created by MAC-OBS-26 on 01/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
